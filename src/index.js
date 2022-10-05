6041ccf4102091f2bd8896339e5f0aa535478c28
mak-somniferum <suhjin700@gmail.com>
2022-10-05 14:25:54 +0900

    .

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.BrowserRouter>
-        <App basename={process.env.PUBLIC_URL} />
+    <React.BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
     </React.BrowserRouter>
 );
 
